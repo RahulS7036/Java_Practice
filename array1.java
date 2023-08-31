68d2cda5dda5a8615443b587a608ca381dfa65fa
Sabale_Rahul <sabaler94@gmail.com>
2023-08-31 09:55:55 +0530

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,21 @@
+# Java_Practice
+//Saved all the code related to java Practice
+class array1 {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;//3
+        int ans[] = new int[2*n];//6
+        int j =0;
+        for(int i = 0; i<ans.length; i++){
+            if( j == n){
+                j = 0; 
+            }
+            ans[i] = nums[j];
+            
+            j++;
+
+        }
+        return ans;
+    }
+}
+//Input: nums = [1,2,1]
+//Output: [1,2,1,1,2,1]
