c56cce22485fe715e132401af3dc17b450085cce
Sabale_Rahul <sabaler94@gmail.com>
2023-09-03 17:09:57 +0530

    Create SmallerandLarger.java
    Input:
    N = 7, X = 0
    Arr[] = {1, 2, 8, 10, 11, 12, 19}
    Output: 0 7
    Explanation: There are no elements less or
    equal to 0 and 7 elements greater or equal
    to 0.

---
+++ SmallerandLarger.java
@@ -0,0 +1,61 @@
+// GFG -> Array -> Smaller and Larger
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class SmallerandLarger {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans[] = new Solve().getMoreAndLess(arr, n, x);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        // code here
+        
+         
+         int[] ans = new int[2];
+         
+         for(int i=0;i<n;i++){
+             if(arr[i] <= x){
+                 ans[0] +=1;
+             }
+             if(arr[i] >= x){
+                 ans[1] +=1;
+             }
+         }
+         return ans; 
+        
+    }
+}
