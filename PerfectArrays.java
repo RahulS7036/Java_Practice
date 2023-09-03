3d88d7ffd577e1196f1c1c77577f9f3c3b7b18a1
Sabale_Rahul <sabaler94@gmail.com>
2023-09-03 16:36:11 +0530

    Create PerfectArrays.java
    Input : Arr[] = {1, 2, 3, 2, 1}
    Output : PERFECT

---
+++ PerfectArrays.java
@@ -0,0 +1,72 @@
+//GFG Array Perfect Arrays Problem
+
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    int n = Integer.parseInt(br.readLine());
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    if(obj.IsPerfect(arr, n))
+		        System.out.println("PERFECT");
+		    else
+		        System.out.println("NOT PERFECT");
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+        // Complete the function
+        
+        boolean flag = true;
+        int[] result = new int[n];
+        int i=n-1;
+        int j=0;
+        while(i >=0){
+            result[j] = a[i];
+            j++;
+            i--;
+        }
+        for(int k=0;k<n;k++){
+            if(a[k] != result[k]){
+                flag = false;
+                break;
+            }
+        }
+        
+        return flag;
+        
+        
+    }
+    
+}
+
