b4604b90efc81681bc7f50881fb9b618b31c2034
Sabale_Rahul <sabaler94@gmail.com>
2023-09-19 22:27:05 +0530

    String
    Input:
    s = "geeks"
    Output: False
    Explanation: The string contains different
    character 'g', 'e', 'k' and 's'.

---
+++ CheckString.java
@@ -0,0 +1,50 @@
+//GFG -> Array -> Check String
+
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class CheckString
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            if (new Sol().check (s))
+                System.out.println ("YES");
+            else
+                System.out.println ("NO");
+        }
+        
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    Boolean check (String s)
+    {
+        // your code here
+        
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) != s.charAt(i-1)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
