2f0bb5edd248249b298952a84c6ee721c463634c
Sabale_Rahul <sabaler94@gmail.com>
2023-09-23 09:00:49 +0530

    String
    Input:
    str = "geeks"
    Output: GEEKS

---
+++ Lowercasetouppercase.java
@@ -0,0 +1,35 @@
+//GFG -> String  -> Lower case to upper case
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class Lowercasetouppercase{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+        while(t-- > 0){
+            String str = read.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.to_upper(str));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String to_upper(String str) 
+    { 
+        // code here
+        //  return str.toUpperCase();
+      String ans ="";
+          
+      for(int i =0 ; i<str.length() ; i++ ){
+          ans = ans+(char)(str.charAt(i)-32);
+        }
+      return ans;
+    }
+} 
